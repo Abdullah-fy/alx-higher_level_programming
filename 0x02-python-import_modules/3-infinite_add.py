88856c2decd5a32ab5251d65fa5fd9f547297bf1
Abdullah-fy <https://github.com/Abdullah-fy/simple_shell.git>
2023-10-06 14:24:08 +0300

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,13 +3,8 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    arglen = len(argv) - 1
-    if arglen == 0:
-        print('0 arguments.')
-    elif arglen == 1:
-        print('1 argument:')
-        print('1: {}'.format(argv[1]))
-    else:
-        print('{} arguments:'.format(arglen))
+    newsum = 0
+    if len(argv) > 1:
         for i in range(1, len(argv)):
-            print('{}: {}'.format(i, argv[i]))
+            newsum += int(argv[i])
+    print(newsum)
