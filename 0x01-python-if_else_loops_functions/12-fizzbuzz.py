2bea06418002f5b937918cc40adf514241a9ce1e
Abdullah-fy <https://github.com/Abdullah-fy/simple_shell.git>
2023-10-08 09:53:25 +0300

    update

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for a in range(1, 101):
-        if a % 5 == 0 and a % 3 == 0:
+    for i in range(1, 101):
+        if i % 5 == 0 and i % 3 == 0:
             print("FizzBuzz ", end="")
-        elif a % 3 == 0:
+        elif i % 3 == 0:
             print("Fizz ", end="")
-        elif a % 5 == 0:
+        elif i % 5 == 0:
             print("Buzz ", end="")
         else:
             print(f"{i} ", end="")
