f5fe9d0d734c5815fd45fbb1ecb1eb82824ed4f6
Abdullah-fy <https://github.com/Abdullah-fy/simple_shell.git>
2023-10-27 01:01:20 +0200

    print num of int

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    for i in range(0, x):
+    cont = 0
+    for iter1 in range(0, x):
         try:
-            print("{:d}".format(my_list[i]), end = '')
-            count += 1
-        except Exception:
+            print("{:d}".format(my_list[iter1]), end='')
+            cont += 1
+        except (TypeError, ValueError, RuntimeError, NameError):
             pass
-    print('')
-    return count
+    print("")
+    return cont
