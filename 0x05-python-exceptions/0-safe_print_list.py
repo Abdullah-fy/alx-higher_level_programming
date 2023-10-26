e8d9927c64dd64f81e1a805312ae6d76ae5fba80
Abdullah-fy <https://github.com/Abdullah-fy/simple_shell.git>
2023-10-27 00:45:02 +0200

    task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in my_list:
-        if count == x :
-            break
+    cont = 0
+    for iter1 in range(0, x):
         try:
-            print(i, end ="")
-            count +=1
+            print(my_list[iter1], end='')
+            cont += 1
         except (IndexError):
             pass
     print("")
-    return count
+    return cont
