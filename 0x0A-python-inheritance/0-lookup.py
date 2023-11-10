c21509f7485022b2c6d3ffcb29c37d5e660c6fd1
Abdullah-fy <https://github.com/Abdullah-fy/simple_shell.git>
2023-11-10 22:16:55 +0200

    task

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-'''Write a function that returns the list of available attributes and methods of an object'''
+"""Defines an object attribute lookup function."""
 
 def lookup(obj):
+    """Return a list of an object's available attributes."""
     return dir(obj)
